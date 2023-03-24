63052c11283a90d8025d1dc43bfcbda2526c776d
Johan Palm <pij-se@users.noreply.github.com>
2023-03-24 23:34:45 +0100

    Update setup.sh
    Added reference to license.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Raspbery Pi 4, HiFiBerry DAC+ ADC, with Icecast2 and Darkice, for local network online radio.
 # Copyright (c) 2023 Johan Palm <johan@pij.se>
 # All rights reserved.
+# Published under the GNU General Public License v3.0.
 
 # Update and upgrade Raspberry Pi OS
 sudo apt update
