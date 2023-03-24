2ac89093fe7b10ae62f1663a1e503ded2a5dbb03
Johan Palm <pij-se@users.noreply.github.com>
2023-03-24 23:31:49 +0100

    Create setup.sh
    Initial commit, work in progress.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# https://github.com/pij-se/hifiberry-dac_plus_adc-online_radio/setup.sh
+# 
+# Raspbery Pi 4, HiFiBerry DAC+ ADC, with Icecast2 and Darkice, for local network online radio.
+# Copyright (c) 2023 Johan Palm <johan@pij.se>
+# All rights reserved.
+
+# Update and upgrade Raspberry Pi OS
+sudo apt update
+sudo apt upgrade
+
+# Install and configure Icecast2
+sudo apt install icecast2
