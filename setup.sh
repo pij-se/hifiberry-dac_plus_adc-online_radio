06d03e4e76395ab703e7fa5f33ff8dcb9c31cfb2
Johan Palm <pij-se@users.noreply.github.com>
2023-03-24 23:57:51 +0100

    Update setup.sh
    Added missing shebang. Added echos.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # https://github.com/pij-se/hifiberry-dac_plus_adc-online_radio/setup.sh
 # 
 # Raspbery Pi 4, HiFiBerry DAC+ ADC, with Icecast2 and Darkice, for local network online radio.
@@ -5,9 +6,12 @@
 # All rights reserved.
 # Published under the GNU General Public License v3.0.
 
-# Update and upgrade Raspberry Pi OS
+# Update Raspberry Pi OS packages
+echo "Checking for updates..."
 sudo apt update
+echo "Downloading and installing updates..."
 sudo apt upgrade
 
 # Install and configure Icecast2
+echo "Installing Icecast2..."
 sudo apt install icecast2
